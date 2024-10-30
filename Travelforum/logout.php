9610ee3e0b6a0acec6a0e58d8c9b2336d0ef1c1c
Võ Hoàng Vũ <hoangvovu907@gmail.com>
2024-10-30 23:34:45 +0700

    update postdetail.php

---
+++ Travelforum/logout.php
@@ -0,0 +1,26 @@
+<?php
+session_start(); // Bắt đầu phiên
+
+// Load file cấu hình
+$config = include('config.php');
+
+// Kết nối cơ sở dữ liệu
+$dbConfig = $config['db'];
+$dsn = "pgsql:host={$dbConfig['host']};port={$dbConfig['port']};dbname={$dbConfig['dbname']}";
+
+try {
+    $conn = new PDO($dsn, $dbConfig['user'], $dbConfig['password']);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Kết nối đến cơ sở dữ liệu thất bại: " . $e->getMessage());
+}
+
+// Hủy bỏ tất cả dữ liệu phiên
+if (isset($_SESSION)) {
+    session_destroy(); // Hủy bỏ session nếu nó tồn tại
+}
+
+// Chuyển hướng đến trang chính hoặc trang mong muốn
+header("Location: index.php");
+exit();
+?>
